63273ead829506106468952a6f85295cd57f9117
J.K.SAGE <sage417@users.noreply.github.com>
2026-07-11 21:34:04 +0800

    Create uci-defaults script for argon theme
    Add script to set default theme for LuCI

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,3 +14,13 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 echo 'CONFIG_PACKAGE_luci-theme-argon=y' >> .config
+
+# set default theme
+mkdir -p package/base-files/files/etc/uci-defaults
+cat > package/base-files/files/etc/uci-defaults/99-theme-argon << EOF
+#!/bin/sh
+uci set luci.main.mediaurlbase=/luci-static/argon
+uci commit luci
+exit 0
+EOF
+chmod +x package/base-files/files/etc/uci-defaults/99-theme-argon
